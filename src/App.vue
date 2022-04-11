a49e06cd54c89a68182a4c0c05f06e356c2788f1
Nemanja <manojlovic911@hotmail.com>
2022-04-11 11:54:13 +0200

    Add Hero section context in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
   <TheHeader />
+  <BaseHero>
+    <h1>Welcome back, Nemanja</h1>
+    <p class="text-gray mb-0 sub-hero-heading">
+      You've got ... resources saved.
+    </p>
+  </BaseHero>
 </template>
 
 <script>
 import TheHeader from "./components/layout/TheHeader.vue";
-
+import BaseHero from "./components/UI/BaseHero.vue";
 export default {
   components: {
     TheHeader,
+    BaseHero,
   },
   data() {},
 };
@@ -17,5 +24,21 @@ export default {
 html,
 body {
   background-color: #f6f6f6;
+}
+
+.sub-hero-heading {
+  margin-top: 21px;
+  font-weight: 700;
+}
+
+p,
+h1,
+h2,
+h3 {
+  color: #0f0f0f;
+}
+
+.text-gray {
+  color: #828282;
 }
 </style>
