bcae9338a033991783f624846da4ff899f2aaad7
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-04-12 05:51:36 +0200

    Create BaseCard comp

---
+++ src/components/UI/BaseCard.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="row base-card">
+    <div class="col-12">
+      <slot name="title"></slot>
+    </div>
+    <div class="col-12">
+      <slot name="body"></slot>
+    </div>
+    <div class="col-12">
+      <BaseTag>
+        <slot name="tag"></slot>
+      </BaseTag>
+    </div>
+    <div class="col-12">
+      <slot name="resource"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import BaseTag from "./BaseTag.vue";
+
+export default {
+  components: {
+    BaseTag,
+  },
+};
+</script>
+
+<style scoped>
+.base-card {
+  background-color: #ffffff;
+  padding: 16px;
+  filter: drop-shadow(0px 1px 1px rgba(0, 0, 0, 0.25));
+  border-radius: 5px;
+}
+</style>
