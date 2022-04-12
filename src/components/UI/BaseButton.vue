0cabd6b7f5aaa96a66d806bab5814bbe617438b9
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-04-12 04:05:34 +0200

    Create BaseButton comp

---
+++ src/components/UI/BaseButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <button class="btn btn-primary">
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+.btn-primary.active {
+  background: #040921;
+}
+
+.btn-primary {
+  padding: 10px 16px;
+  border-radius: 8px;
+  background: rgba(4, 9, 33, 0.32);
+  border: none;
+}
+
+.btn-primary:hover {
+  background: #040921;
+}
+</style>
