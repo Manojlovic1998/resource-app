529bed507f3f830f29c2a3536906ae33a2381788
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-04-12 04:05:59 +0200

    Create TheOptions comp section

---
+++ src/components/layout/TheOptions.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container text-center options-wrapper">
+    <BaseButton class="me-4">Stored Resources </BaseButton>
+    <BaseButton>Add Resource </BaseButton>
+  </div>
+</template>
+
+<script>
+import BaseButton from "../UI/BaseButton.vue";
+
+export default {
+  components: {
+    BaseButton,
+  },
+};
+</script>
+
+<style>
+.options-wrapper {
+  margin-top: 34px;
+}
+</style>
