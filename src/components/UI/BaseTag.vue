dce0b7ee589f6cafdef5a9ebe756fcc6c8dc9f4c
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-04-12 05:52:09 +0200

    Create BaseTag component

---
+++ src/components/UI/BaseTag.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="tag-wrapper mb-3">
+    <slot></slot>
+  </div>
+</template>
+
+
+<style scoped>
+.tag-wrapper {
+  display: inline-block;
+  padding: 6px 12px;
+  border-radius: 8px;
+  border: 2px solid rgba(4, 9, 33, 0.32);
+}
+</style>
