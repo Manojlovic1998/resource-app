3f5a843ec21dc89b9a1a5417b15fe9949ad0312d
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-04-12 05:52:40 +0200

    Create TheResources section component

---
+++ src/components/layout/TheResources.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container resources-wrapper">
+    <BaseCard v-for="resource in resources" :key="resource.id">
+      <template v-slot:title
+        ><p class="fw-bolder">{{ resource.title }}</p></template
+      >
+      <template v-slot:body
+        ><p class="text-gray">{{ resource.body }}</p></template
+      >
+      <template v-slot:tag
+        ><p class="mb-0 tag">{{ resource.tag }}</p></template
+      >
+      <template v-slot:resource
+        ><a :href="resource.resource" class="fw-bold"
+          >View Resource</a
+        ></template
+      >
+    </BaseCard>
+  </div>
+</template>
+
+<script>
+import BaseCard from "../UI/BaseCard.vue";
+
+export default {
+  props: ["resources"],
+  components: {
+    BaseCard,
+  },
+};
+</script>
+
+<style scoped>
+.resources-wrapper {
+  margin-top: 52px;
+}
+
+.tag {
+  color: #686868;
+  font-weight: 600;
+}
+
+a {
+  text-decoration: none;
+}
+</style>
